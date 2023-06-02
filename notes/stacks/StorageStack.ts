a7d5e1d9e5188c919afae2e25c44bc150009a1ee
davidtaing <adavidtaing@gmail.com>
2023-06-02 20:02:15 +1000

    Adding a storage stack

---
+++ notes/stacks/StorageStack.ts
@@ -0,0 +1,17 @@
+import { Bucket, StackContext, Table } from "sst/constructs";
+
+export function StorageStack({ stack, app }: StackContext) {
+  const table = new Table(stack, "Notes", {
+    fields: {
+      userId: "string",
+      noteId: "string",
+    },
+    primaryIndex: { partitionKey: "userId", sortKey: "noteId" },
+  });
+  const bucket = new Bucket(stack, "Uploads");
+
+  return {
+    table,
+    bucket,
+  };
+}
